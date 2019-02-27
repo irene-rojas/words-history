f3a831c45d19d720d233fb35c59664d3b73e1f80
Irene R <irene@smallwondermedia.com>
2019-02-26 21:39:06 -0500

    env

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+const API_KEY = process.env.REACT_APP_MW_API_KEY;
+
 class App extends Component {
+
+    state = {
+        results: []
+    }
+
+
+
   render() {
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        {this.state.results}
+
+
       </div>
     );
   }
