77b4b465ad883c2a0f07ed0444b4623bec00f5ee
Irene R <irene@smallwondermedia.com>
2019-02-27 00:17:05 -0500

    still probing api

--- src/App.js
+++ src/App.js
@@ -11,28 +11,27 @@ class App extends Component {
     }
 
     componentDidMount() {
-        const API_KEY = process.env.REACT_APP_MW_API_KEY;
-
-        axios.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/cat?key=${API_KEY}`)
+        // const API_KEY = process.env.REACT_APP_MW_API_KEY;
+        axios.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/cat?key=${process.env.REACT_APP_MW_API_KEY}`)
         .then(res => {
             this.setState({
-                results: res.toString()
+                results: res.data
             })
-        })
+        });
         console.log(this.state.results);
     }
 
-  render() {
 
+  render() {
 
     return (
       <div className="App">
 
         {/* <h1>{this.state.results}</h1> */}
 
-        {this.state.results.map((index, result) => 
+        {/* {this.state.results.map((index, result) => 
             <h1 key={index}>{result.hwi}</h1>
-        )}
+        )} */}
 
       </div>
     );
