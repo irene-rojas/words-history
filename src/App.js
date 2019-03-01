251e26f87687d0f8f59b636ca5b86bdc7b4e72a9
Irene R <irene@smallwondermedia.com>
2019-02-28 21:47:35 -0500

    comments

--- src/App.js
+++ src/App.js
@@ -11,9 +11,11 @@ class App extends Component {
     }
 
     componentDidMount() {
-        axios.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/winter?key=${process.env.REACT_APP_MW_API_KEY}`)
+        // but how choose a random word from the dictionary?
+        axios.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/speaker?key=${process.env.REACT_APP_MW_API_KEY}`)
         .then(res => {
             (console.log(res.data[0].def[0].sseq[0][0][1].dt[0][1]))
+            // if word does not have a def at this point, ignore it and select a different word. repeat until necessary
         });
     }
 
