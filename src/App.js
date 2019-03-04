372a98ae1e9c74992fbc7c2510576913bb73e428
Irene R <irene@smallwondermedia.com>
2019-03-03 21:32:09 -0500

    replace it but not /it

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
         axios.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/cat?key=${process.env.REACT_APP_MW_API_KEY}`)
         .then(res => {
             const result = res.data[0].def[0].sseq[0][0][1].dt[0][1];   // shortdef
-            const displayResult = result.replace(/{bc}/gi, " ");
+            const displayResult = result.replace(/{bc}|{it}/gi, "");
             console.log(displayResult);
             // if word does not have a def at this point, ignore it and select a different word. repeat until necessary
             // need to ignore {} or other symbols 
