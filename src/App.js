2665d1400af590d7dda641535eefaa892dca2935
Irene R <irene@smallwondermedia.com>
2019-03-03 23:15:44 -0500

    synced words and defs

--- src/App.js
+++ src/App.js
@@ -2,27 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 
-// const words = ["baggage", "fan", "charge", "computer", "monitor", "keyboard", "news", "space", "fare", "camera", "cable", "boarder", "dog", "robot", "comma", "mug", "bow", "arow", "row", "arrow", "screen", "sound", "mail", "stop", "travel", "program", "light", "remote", "contact", "adventure", "journey", "passage", "react", "under", "tire", "support", "brave", "report", "attain", "achieve", "respond", "attach", "quirk", "expect", "await", "stay", "room", "space", "period", "ranged", "incline", "recline", "fade", "lose", "accord", "render", "supply", "win", "property"];
+const words = ["baggage", "fan", "charge", "computer", "monitor", "keyboard", "news", "space", "fare", "camera", "cable", "boarder", "dog", "robot", "comma", "mug", "bow", "arow", "row", "arrow", "screen", "sound", "mail", "stop", "travel", "program", "light", "remote", "contact", "adventure", "journey", "passage", "react", "under", "tire", "support", "brave", "report", "attain", "achieve", "respond", "attach", "quirk", "expect", "await", "stay", "room", "space", "period", "ranged", "incline", "recline", "fade", "lose", "accord", "render", "supply", "win", "property", "forth", "shatter", "interest", "ring", "chain"];
 
 class App extends Component {
 
     state = {
-        // word: "",
-        etResult: "",
+        word: "",
+        def: ""
         // altResult: ""
     }
 
     componentDidMount() {
+        let word = words[Math.floor(Math.random() * words.length)];
+        console.log(word);
+        this.setState({
+            word: word,
+        });
         // but how choose a random word from the dictionary?
-        axios.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/property?key=${process.env.REACT_APP_MW_API_KEY}`)
+        axios.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.REACT_APP_MW_API_KEY}`)
         .then(res => {
             const result = res.data[0].def[0].sseq[0][0][1].dt[0][1];   // shortdef
             // const displayResult = result.replace(/{bc}|{it}/gi, "");
             const displayResult = result.replace(/{bc}|{it}|a_link|d_link|sx/gi, "").replace(/[^a-zA-Z0-9(*), ]/gi, "");  //1st replace: specific exclusions. 2nd replace: protected items
+            this.setState({
+                def: displayResult
+            });
             console.log(displayResult);
             // if word does not have a def at this point, ignore it and select a different word. repeat until necessary
             // need to ignore {} or other symbols 
         });
+
     }
 
 
@@ -30,6 +39,14 @@ class App extends Component {
 
     return (
       <div className="App">
+
+        <div> 
+            {this.state.word}
+        </div>
+
+        <div>
+            {this.state.def}
+        </div>
 
       </div>
     );
