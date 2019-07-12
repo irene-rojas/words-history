7c79d98217e81e00f36dd12df704250cffe4d437
Irene Rojas <irene@smallwondermedia.com>
2019-07-11 22:07:43 -0400

    Updates

---
+++ precache-manifest.0a494c448bb627e80ef9327338b8e782.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "765f606063239d0a8597644323089368",
+    "url": "/word-guess/static/media/mwlogo.765f6060.png"
+  },
+  {
+    "revision": "58143a1efd389ec49989",
+    "url": "/word-guess/static/js/runtime~main.58143a1e.js"
+  },
+  {
+    "revision": "ab19367bbe8e6dddbfed",
+    "url": "/word-guess/static/js/main.ab19367b.chunk.js"
+  },
+  {
+    "revision": "4bdbd4d346960aa59427",
+    "url": "/word-guess/static/js/2.4bdbd4d3.chunk.js"
+  },
+  {
+    "revision": "ab19367bbe8e6dddbfed",
+    "url": "/word-guess/static/css/main.faee11b7.chunk.css"
+  },
+  {
+    "revision": "de6d85baf06c0e1cad44efd9cc769660",
+    "url": "/word-guess/index.html"
+  }
+];
